bb495e17ba5ad0fb46787036a11328ba3da20d24
ilikepizza107 <twachtveitl107@gmail.com>
2022-02-08 20:49:12 -0600

    Stagelist Toggle Attempt 2
    Almost, netplay code menu doesn't work

--- Build/Project+/Source/Extras/More.asm
+++ Build/Project+/Source/Extras/More.asm
@@ -9,11 +9,10 @@
 Set random stagelist based on Code Menu Stagelist setting (ILPM) [Bird]
 ############################################################################
 * 20523400 00000000 # If Code Menu stagelist is set to 0
-* 20523320 00000000 # If 80523320 is 00000000
-* 04523320 DEADBEEF # Set it to DEADBEEF
-* 04523310 00000000 # Set all other values to 00000000
+* 20523310 00000000 # If 80523320 is 00000000
+* 04523310 DEADBEEF # Set it to DEADBEEF
+* 04523320 00000000 # Set all other values to 00000000
 * 04523330 00000000
-* 04523340 00000000
 * 42000000 90000000 #Set base address to 90000000
 * 0417BE74 14600115 # Brawl stages
 * 0417BE70 00021000 # Melee stages
@@ -23,11 +22,10 @@ Set random stagelist based on Code Menu Stagelist setting (ILPM) [Bird]
 Set random stagelist based on Code Menu Stagelist setting (Middle 3) [Bird]
 ############################################################################
 * 20523400 00000001 # If Code Menu stagelist is set to 1
-* 20523310 00000000 # If 80523310 is 00000000
-* 04523310 DEADBEEF # Set it to DEADBEEF
-* 04523320 00000000 # Set all other values to 00000000
+* 20523320 00000000 # If 80523310 is 00000000
+* 04523320 DEADBEEF # Set it to DEADBEEF
+* 04523310 00000000 # Set all other values to 00000000
 * 04523330 00000000
-* 04523340 00000000
 * 42000000 90000000 #Set base address to 90000000
 * 0417BE74 04000001 # Brawl stages
 * 0417BE70 00020000 # Melee stages
@@ -37,12 +35,11 @@ Set random stagelist based on Code Menu Stagelist setting (Middle 3) [Bird]
 Set random stagelist based on Code Menu Stagelist setting (PMBR) [Bird]
 ############################################################################
 * 20523400 00000002 # If Code Menu stagelist is set to 2
-* 20523320 00000000 # If 80523320 is 00000000
-* 04523320 DEADBEEF # Set it to DEADBEEF
+* 20523330 00000000 # If 80523320 is 00000000
+* 04523330 DEADBEEF # Set it to DEADBEEF
 * 04523310 00000000 # Set all other values to 00000000
-* 04523330 00000000
-* 04523340 00000000
+* 04523320 00000000
 * 42000000 90000000 #Set base address to 90000000
 * 0417BE74 15200017 # Brawl stages
 * 0417BE70 00021000 # Melee stages
-* E0000000 80008000
+* E0000000 80008000 
--- Build/Project+/Source/Project+/FilePatchCode.asm
+++ Build/Project+/Source/Project+/FilePatchCode.asm
@@ -279,19 +279,7 @@ string "stageslot/"                     @ $80550C98 # This address might change 
 string "stageinfo/"                     @ $80550CA8 # This address might change and crash
 * E0000000 80008000
 
-* 24523400 00000000 # If 80523400 is greater to 0, and also
-* 22523400 00000002 # If 80523400 is not equal to 2
-string "/menu2/mu_menumain.pac"         @ $806FB248
-string "mu_menumain_en.pac"             @ $817F62BC
-string "/menu2/sc_selcharacter2.pac"    @ $806FF308
-string "sc_selcharacter2_en.pac"        @ $817F634D
-string "/menu2/br_selmap.pac"           @ $806FF3F0
-string "br_selmap_en.pac"               @ $817F637C
-string "stageslotbr/"                   @ $80550C98 # This address might change and crash
-string "stageinfobr/"                   @ $80550CA8 # This address might change and crash
-* E0000000 80008000
-
-* 20523400 00000002 # If 80523400 is equal to 2
+* 24523400 00000000 # If 80523400 is greater to 0
 string "/menu2/mu_menumain.pac"         @ $806FB248
 string "mu_menumain_en.pac"             @ $817F62BC
 string "/menu2/sc_selcharacter2.pac"    @ $806FF308

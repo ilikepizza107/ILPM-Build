23cfd1163f2439197c38f822b2b9bf88a179036c
ilikepizza107 <twachtveitl107@gmail.com>
2022-10-07 18:35:30 -0500

    Update to version 2.0
    Big update

--- Build/Project+/Source/Project+/CSE.asm
+++ Build/Project+/Source/Project+/CSE.asm
@@ -497,3 +497,80 @@ discFileSkip:
 notCustomMusic:
   lwz r12, 0(r29)							# Original operation
 }
+
+#########################################
+Song Title Displays On Pause Screen [Eon]
+#########################################
+HOOK @ $800F0938
+{
+bctrl
+lis r3, 0x805A
+lwz r3, 0x2D0(r3)
+lwz r3, 0x44(r3)
+lwz r5, 0x40(r3)
+lwz r5, 0x10(r5)
+
+lwz r3, 0x0(r31)
+lwz r4, 0xE4(r3)
+lwz r12, 0x0(r3)
+lwz r12, 0x34(r12)
+mtctr r12
+bctrl 
+}```
+#########################################
+B while holding A + paused will re-roll stage music [Eon]
+#########################################
+HOOK @ $800F1680 #if B pressed in pause screen
+{
+    lwz r0, 0x8(r1)
+    andi. r0, r0, 0x100 #if A is already held 
+    beq original
+    #getSceneManager
+    lis r12, 0x8002
+    ori r12, r12, 0xD018
+    mtctr r12 
+    bctrl 
+    lwz r4, 0x4(r3)
+    lwz r4, 0x40(r4) #stOperatorBgm
+    lwz r31, 0x44(r4) #song ID pointer thing
+ 
+    #sndBgmRateSystem.prepareBGM(stageId, 0, 0)
+    lis r3, 0x805A 
+    lwz r3, 0x1D8(r3) #sndBgmRateSystem
+    lwz r4, 0x48(r4) #stageId 
+    li r5, 0 #unsure 
+    li r6, 0 #song ID, if 0 it picks a new random one 
+    lis r12, 0x8007
+    ori r12, r12, 0x9104
+    mtctr r12 
+    bctrl 
+    #store selected song in appropriate place 
+    lbz r4, 0x5C(r31)
+    stw r3, 0x5C(r31)
+    stb r4, 0x5C(r31)
+ 
+playSong:
+    #sndBgmRateSystem.startBGM(0)
+    lis r3, 0x805A
+    lwz r3, 0x1D8(r3)
+    li r4, 0
+    lis r12, 0x8007
+    ori r12, r12, 0x91D4
+    mtctr r12 
+    bctrl 
+exit:
+    #restore original r31
+    mr r3, r29
+    lis r12, 0x800F
+    ori r12, r12, 0x00AC
+    mtctr r12 
+    bctrl 
+    mr r31, r3
+    #exit
+    lis r12, 0x800f
+    ori r12, r12, 0x1698
+    mtctr r12 
+    bctr 
+original:
+    lbz r0, 0x64(r29)
+}
